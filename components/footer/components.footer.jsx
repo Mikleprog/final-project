e69118cde5bbb732d64712a3d547db072f38e3e1
Mikhail Moshnikov <62326146+Mikleprog@users.noreply.github.com>
2020-05-30 00:07:07 +0300

    Create components.footer.jsx

---
+++ components/footer/components.footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './footer.css';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p className="contacts">
+            +7(921) 000-00-00
+        <br/>
+           "EVERY DAY FROM 10:00 to 22:00"
+         <br/> 
+         </p>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
